9666e01c4abb5f23ee1b9551da64da1c2425cb81
Monica <a01707439@itesm.mx>
2024-01-15 09:12:28 +0100

    Removed reproductivity for testing songs

--- main.py
+++ main.py
@@ -17,18 +17,16 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import os
 
-np.random.seed(45)
-
 ## BEGINING OF LOADING THE DATA 
 
 filepath = "training_data/Piano/" 
 
-all_midis= []
+piano_files= []
 for i in os.listdir(filepath):
     if i.endswith(".mid"):
         tr = filepath+i
         midi = converter.parse(tr)
-        all_midis.append(midi)
+        piano_files.append(midi)
             
 def extract_notes(file):
     notes = []
@@ -45,16 +43,15 @@ def extract_notes(file):
 
     return notes
 
-Corpus= extract_notes(all_midis)
+Corpus= extract_notes(piano_files)
 print("# of Notes in the piano dataset:", len(Corpus))
 
 def chords_n_notes(Snippet):
     Melody = []
-    offset = 0 #Incremental
+    offset = 0 
     for i in Snippet:
-        #If it is chord
         if ("." in i or i.isdigit()):
-            chord_notes = i.split(".") #Seperating the notes in chord
+            chord_notes = i.split(".") 
             notes = [] 
             for j in chord_notes:
                 inst_note=int(j)
@@ -63,34 +60,34 @@ def chords_n_notes(Snippet):
                 chord_snip = chord.Chord(notes)
                 chord_snip.offset = offset
                 Melody.append(chord_snip)
-        # pattern is a note
         else: 
             note_snip = note.Note(i)
             note_snip.offset = offset
             Melody.append(note_snip)
-        # increase offset each iteration so that notes do not stack
         offset += 1
-    Melody_midi = stream.Stream(Melody)   
-    return Melody_midi
+    melody_stream = stream.Stream(Melody)   
+    return melody_stream
 
-#Creating a count dictionary
 count_num = Counter(Corpus)
 print("Total unique notes in the Corpus:", len(count_num))
 
 Notes = list(count_num.keys())
 Recurrence = list(count_num.values())
-print("Average recurrenc for a note in Corpus:", sum(Recurrence) / len(Recurrence))
-print("Most frequent note in Corpus appeared:", max(Recurrence), "times")
-print("Least frequent note in Corpus appeared:", min(Recurrence), "time")
+print("Average appearance of one note - ", sum(Recurrence) / len(Recurrence))
+print("Note that appeared the most - ", max(Recurrence), "times")
+print("Note that appeared the least - ", min(Recurrence), "time")
 
-# Plotting the distribution of Notes
-plt.figure(figsize=(18,3),facecolor="#97BACB")
-bins = np.arange(0,(max(Recurrence)), 50) 
-plt.hist(Recurrence, bins=bins, color="#97BACB")
-plt.axvline(x=100,color="#DBACC1")
-plt.title("Frequency Distribution Of Notes In The Corpus")
-plt.xlabel("Frequency Of Chords in Corpus")
-plt.ylabel("Number Of Chords")
+sns.set_style("whitegrid")
+
+plt.figure(figsize=(12, 6), facecolor="#FFD1DC")
+bins = np.arange(0, len(Recurrence) + 1, 1)
+plt.hist(range(len(Recurrence)), bins=bins, weights=Recurrence, color="#CF578A", edgecolor="white", alpha=0.7)
+plt.axhline(y=100, color="#DBACC1", linestyle="--", linewidth=2, label="Threshold")
+plt.title("Frequency of Notes in the Corpus", fontsize=16)
+plt.xlabel("Note ID (Number associated to note)", fontsize=12)
+plt.ylabel("Times it appears in the Corpus", fontsize=12)
+plt.legend()
+plt.grid(axis="x", linestyle="--", alpha=0.7)
 plt.show()
 
 rare_note = []
@@ -99,11 +96,11 @@ for index, (key, value) in enumerate(count_num.items()):
         m =  key
         rare_note.append(m)
         
-print("Total number of notes that occur less than 100 times:", len(rare_note))
-
 for element in Corpus:
     if element in rare_note:
         Corpus.remove(element)
+        
+print("Notes that appear less than 100 times - ", len(rare_note))
 
 print("Length of Corpus after elemination the rare notes:", len(Corpus))
 
@@ -111,18 +108,16 @@ print("Length of Corpus after elemination the rare notes:", len(Corpus))
 
 symb = sorted(list(set(Corpus)))
 
-L_corpus = len(Corpus) #length of corpus
-L_symb = len(symb) #length of total unique characters
+L_corpus = len(Corpus) 
+L_symb = len(symb) 
 
-#Building dictionary to access the vocabulary from indices and vice versa
 mapping = dict((c, i) for i, c in enumerate(symb))
 reverse_mapping = dict((i, c) for i, c in enumerate(symb))
 
-print("Total number of characters:", L_corpus)
-print("Number of unique characters:", L_symb)
+print("Characters in Corpus - " , L_corpus)
+print("Unique characters in Corpus - " , L_symb)
 
-#Splitting the Corpus in equal length of strings and output target
-length = 40
+length = 40 # Can be modified
 features = []
 targets = []
 for i in range(0, L_corpus - length, 1):
@@ -132,16 +127,12 @@ for i in range(0, L_corpus - length, 1):
     targets.append(mapping[target])
     
 L_datapoints = len(targets)
-print("Total number of sequences in the Corpus:", L_datapoints)
+print("Sequences in the Corpus - ", L_datapoints)
 
-# reshape X and normalize
 X = (np.reshape(features, (L_datapoints, length, 1)))/ float(L_symb)
-
-# one hot encode the output variable
 y = tf.keras.utils.to_categorical(targets) 
 
-#Taking out a subset of data to be used as seed
-X_train, X_seed, y_train, y_seed = train_test_split(X, y, test_size=0.6, random_state=45)
+train_chords, chords_seed, train_target, target_seed = train_test_split(X, y, test_size=0.6)
 
 ## BEGINING OF MODEL IMPLEMENTAITON
 model_file_path = "saved_model.keras"
@@ -162,24 +153,25 @@ else:
     opt = Adamax(learning_rate=0.01)
     model.compile(loss='categorical_crossentropy', optimizer=opt) 
 
-    history = model.fit(X_train, y_train, batch_size=150, epochs=200)
+    history = model.fit(train_chords, train_target, batch_size=150, epochs=200)
     model.save(model_file_path)
     
-    history_df = pd.DataFrame(history.history)
-    fig = plt.figure(figsize=(15,4), facecolor="#97BACB")
-    fig.suptitle("Learning Plot of Model for Loss")
-    pl=sns.lineplot(data=history_df["loss"],color="#444160")
-    pl.set(ylabel ="Training Loss")
-    pl.set(xlabel ="Epochs")
+    # Plotting the learning plot for model loss
+    plt.figure(figsize=(15, 4), facecolor="#FFD1DC")
+    plt.title("Learning Evaluation: Loss over epoch", fontsize=16)
+    sns.lineplot(data=history_df["loss"], color="#CF578A", linewidth=2)
+    plt.ylabel("Training Loss", fontsize=12)
+    plt.xlabel("Number of epoch", fontsize=12)
+    plt.grid(axis="y", linestyle="--", alpha=0.7)
     plt.show()
 
 model.summary()
 
 from music21 import stream
 
-def Malody_Generator(model, X_seed, reverse_mapping, length, L_symb, Note_Count, user_input, normal):
+def Malody_Generator(model, chords_seed, reverse_mapping, length, L_symb, Note_Count, user_input, normal):
     
-    seed = X_seed[np.random.randint(0, len(X_seed) - 1)]
+    seed = chords_seed[np.random.randint(0, len(chords_seed) - 1)]
     Music = ""
     Notes_Generated = []
     
@@ -192,7 +184,7 @@ def Malody_Generator(model, X_seed, reverse_mapping, length, L_symb, Note_Count,
     for i in range(Note_Count):
         seed = seed.reshape(1, length, 1)
         prediction = model.predict(seed, verbose=0)[0]
-        prediction = np.log(prediction) / 1.5  # diversity
+        prediction = np.log(prediction) / 1.5  # Can be changed
         exp_preds = np.exp(prediction)
         prediction = exp_preds / np.sum(exp_preds)
         index = np.argmax(prediction)
@@ -206,8 +198,8 @@ def Malody_Generator(model, X_seed, reverse_mapping, length, L_symb, Note_Count,
     return Music, Melody
 
 user_input = [0,13,128,0,13,12,11,18,1,2,0,13,128,0,13,12,11,18,1,2,0,13,128,0,13,12,11,18,1,2,0,13,128,0,13,12,11,18,1,2,0]
-generated_music, generated_melody = Malody_Generator(model, X_seed, reverse_mapping, length, L_symb, 100, user_input, 0)
-generated_music_normal, generated_melody_normal = Malody_Generator(model, X_seed, reverse_mapping, length, L_symb, 100, [], 1)
+generated_music, generated_melody = Malody_Generator(model, chords_seed, reverse_mapping, length, L_symb, 100, user_input, 0)
+generated_music_normal, generated_melody_normal = Malody_Generator(model, chords_seed, reverse_mapping, length, L_symb, 100, [], 1)
 generated_melody.write('midi','Melody_Generated.mid')
 generated_melody_normal.write('midi','Melody_Generated_1.mid')
 
@@ -215,17 +207,20 @@ generated_melody_normal.write('midi','Melody_Generated_1.mid')
 
 def play_music(file):
     b = music21.converter.parse(file)
-        
+
     import random
     keyDetune = []
     for i in range(127):
         keyDetune.append(random.randint(-30, 30))
-    
-    for n in b.flatten().notes:
-        n.pitch.microtone = keyDetune[n.pitch.midi]
-    
+
+    for n in b.flatten().notesAndRests:
+        if isinstance(n, note.Note):
+            n.pitch.microtone = keyDetune[n.pitch.midi]
+        elif isinstance(n, chord.Chord):
+            for note_in_chord in n.pitches:
+                note_in_chord.microtone = keyDetune[note_in_chord.midi]
+
     sp = music21.midi.realtime.StreamPlayer(b)
     sp.play()
     
 play_music("Melody_Generated_1.mid")
-play_music("Melody_Generated.mid")
